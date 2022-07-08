b40695404bee1d696fe9b27e501a246991233ca9
Dante Durant <dantedurant@Dantes-MacBook-Air.local>
2022-07-08 11:48:37 +0200

    first trage setup of cms

--- payload.config.ts
+++ payload.config.ts
@@ -1,14 +1,18 @@
-import { buildConfig } from 'payload/config';
-import dotenv from 'dotenv';
-import Page from './collections/Page';
-import Media from './collections/Media';
+import { buildConfig } from "payload/config";
+import dotenv from "dotenv";
+import Page from "./collections/Page";
+import Media from "./collections/Media";
+import FormSubmission from "./collections/FormSubmissions";
+import Study from "./collections/Study";
+import Catergory from "./collections/Category";
+import MegaMenu from "./globals/MegaMenu";
+import SocialMedia from "./globals/SocialMedia";
+import Footer from "./globals/Footer";
 
 dotenv.config();
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
-  collections: [
-    Page,
-    Media,
-  ],
+  collections: [Page, Study, Media, FormSubmission, Catergory],
+  globals: [MegaMenu, SocialMedia, Footer],
 });
